b12d7635b0dd343929a7d677c986504c9d366d0b
darudarudarudarudaru <handaru279@gmail.com>
2023-11-04 09:40:42 -0700

    first pull

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index(){
+        return view('dashboard_yogi');
+    }
+
+    public function form_ajukan_cuti(){
+        return view('pengajuanCuti_fandi');
+    }
+
+    public function dashboard_Hr(){
+        return view('dashboard_gilang_hrd_pending');
+    }
+
+    public function dashboard_approved(){
+        return view('dashboard_siti_approved');
+    }
+
+    public function dashboard_reject(){
+        return view('rejectDetail_faiz');
+    }
+}
