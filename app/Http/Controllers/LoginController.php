c42fb6c4486031fd28ca4c4e18730085b66f46cf
darudarudarudarudaru <handaru279@gmail.com>
2023-11-04 12:20:05 -0700

    perubahan logic login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class LoginController extends Controller
-{
+class LoginController extends Controller{
+
     public function index(){
         return view('login_adzmi');
     }
+
     public function logout(){
         return view('logout');
+    }
+
+    public function auth(Request $request){
+        $username = $request->username;
+        $password = $request->password;
+
+        if ($username == 'admin' && $password == 'admin'){
+            return redirect('/home');
+        }elseif ($username == 'user' && $password == 'user'){
+            return redirect('/dashboard_Hr');
+        }else{
+            return "kombinasi password dan username salah";
+        }
     }
 }
