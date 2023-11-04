15c05afae6365705bfcc52e6be63c756254066b2
Rifal2 <adzmirifalsetiawan@gmail.com>
2023-11-04 10:02:02 -0700

    login controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,8 +9,4 @@ class LoginController extends Controller
     public function index(){
         return view('login-azmi');
     }
-    
-    public function logout(){
-        return view('logout');
-    }
 }
