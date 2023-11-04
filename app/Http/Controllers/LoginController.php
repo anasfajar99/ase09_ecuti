0e4e6fc76bcfcf295b1f8f3fe217aa0cd0c4b3b9
mynameFun <panggilaja.valz@gmail.com>
2023-11-04 12:20:19 +0700

    Perubahan Logic Login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,5 +9,16 @@ class LoginController extends Controller
     public function index(){
         return view('login_adzmi');
     }
-    //
+    public function auth(Request $request){
+        $username = $request->username;
+        $password = $request->password;
+
+        if($username == 'admin' && $password == 'admin'){
+            return redirect('/home');
+        }elseif($username == 'HRD' && $password == '123'){
+            return redirect('/dashboard_Hr');
+        }else{
+            return "kombinasi password dan username salah";
+        }
+    }
 }
