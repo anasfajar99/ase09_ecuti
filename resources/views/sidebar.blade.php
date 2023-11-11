f8c0fe839575ef972e26b46fbac577f4c4040f13
FaizRestu <restucahyadi0@gmail.com>
2023-11-11 13:04:39 +0700

    Pertemuan 11 new1

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -17,9 +17,8 @@
                                 <a class="nav-link" href="#" aria-expanded="false" data-target="#submenu-10" aria-controls="submenu-10"><i class="fas fa-calendar-alt"></i>Pengajuan Cuti</a>
                             </li>
                             @elseif (session('akses') == 2)
-                            <li class="nav-item">
-                                <a class="nav-link" href="#" aria-expanded="false" data-target="#submenu-10" aria-controls="submenu-10"><i class="fas fa-user"></i>HRD Dashboard</a>
-                                <li class="nav-item">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-2"  aria-controls="submenu-2"><i class="fa fa-fw fa-chart-pie"></i>HRD Dashboard</a>
                                 <div id="submenu-2" class="collapse submenu" style="">
                                     <ul class="nav flex-column">
                                         <li class="nav-item">
@@ -35,9 +34,8 @@
                                 </div>
                             </li>
                             @elseif (session('akses') == 3)
-                            <li class="nav-item">
-                                <a class="nav-link" href="#" aria-expanded="false" data-target="#submenu-10" aria-controls="submenu-10"><i class="fas fa-user"></i>HRD Dashboard</a>
-                                <li class="nav-item">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-2"  aria-controls="submenu-2"><i class="fa fa-fw fa-chart-pie"></i>HRD Dashboard</a>
                                 <div id="submenu-2" class="collapse submenu" style="">
                                     <ul class="nav flex-column">
                                         <li class="nav-item">
